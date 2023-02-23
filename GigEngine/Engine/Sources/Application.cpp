4294b6e86f36a8b80dd7a6b7f26976c04d500957
Ross <a.goes@student.isartdigital.com>
2023-02-23 13:14:11 -0500

    create new branch

--- GigEngine/Engine/Sources/Application.cpp
+++ GigEngine/Engine/Sources/Application.cpp
@@ -14,31 +14,31 @@ GLint viewPosLocation;
 
 Application::Application()
 {
-    InitOpenGl();
-    window.Init(3,3);
-    editorCamera.SetRatio(window.GetRatio());
-    InitGlad();
+	InitOpenGl();
+	window.Init(3, 3);
+	editorCamera.SetRatio(window.GetRatio());
+	InitGlad();
 
-    //to remove =====================================================
-    VertexShader mainVertex("Resources/Shaders/vert.vert");
-    FragmentShader mainFragment("Resources/Shaders/frag.frag");
+	//to remove =====================================================
+	VertexShader mainVertex("Resources/Shaders/vert.vert");
+	FragmentShader mainFragment("Resources/Shaders/frag.frag");
 
-    mainShader.Link(mainVertex, mainFragment);
+	mainShader.Link(mainVertex, mainFragment);
 
-    viewProjLocation = glGetUniformLocation(mainShader.GetId(), "viewProj");
-    ModelLocation = glGetUniformLocation(mainShader.GetId(), "model");
-    viewPosLocation = glGetUniformLocation(mainShader.GetId(), "viewPos");
+	viewProjLocation = glGetUniformLocation(mainShader.GetId(), "viewProj");
+	ModelLocation = glGetUniformLocation(mainShader.GetId(), "model");
+	viewPosLocation = glGetUniformLocation(mainShader.GetId(), "viewPos");
 
-    GameObject* base = new GameObject();
-    Model* model = new Model(base, "Resources/Models/sponza.obj");
-    base->transform.SetScale(lm::FVec3(0.01f));
-    base->AddComponent(model);
-    testComponent* test = new testComponent(base);
-    base->AddComponent(test);
-    GameObjectManager::AddGameObject(base);
-    //==================================================================
+	GameObject* base = new GameObject();
+	Model* model = new Model(base, "Resources/Models/sponza.obj");
+	base->transform.SetScale(lm::FVec3(0.01f));
+	base->AddComponent(model);
+	testComponent* test = new testComponent(base);
+	base->AddComponent(test);
+	GameObjectManager::AddGameObject(base);
+	//==================================================================
 
-    glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 }
 
 Application::~Application()
@@ -47,82 +47,83 @@ Application::~Application()
 
 Window& Application::GetWindow()
 {
-    return window;
+	return window;
 }
 
 EditorCamera& Application::GetEditorCamera()
 {
-    return editorCamera;
+	return editorCamera;
 }
 
 void Application::Run()
 {
-    glfwPollEvents();
-    window.ProcessInput();
-    Time::UpdateDeltaTime();
-    Draw();
+	glfwPollEvents();
+	window.ProcessInput();
+	Time::UpdateDeltaTime();
+	Draw();
 }
 
 void Application::SwapFrames()
 {
-    glfwSwapBuffers(window.GetGLFWWindow());
+	glfwSwapBuffers(window.GetGLFWWindow());
 }
 
 void Application::InitOpenGl()
 {
-    if (glfwInit() == GLFW_FALSE) {
-        std::cout << "Failed to initialize GLFW" << std::endl;
-    }
+	if (glfwInit() == GLFW_FALSE) {
+		std::cout << "Failed to initialize GLFW" << std::endl;
+	}
 }
 
 void Application::InitGlad()
 {
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        glfwTerminate();
-    }
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+	}
 }
 
 void Application::Draw()
 {
-    ClearWindow();
-    
-    if (isEditor)
-    {
-        editorCamera.Update();
-        UpdateUniforms();
-        UpdateGameObjects();
-    }
+	ClearWindow();
+
+	if (isEditor)
+	{
+		editorCamera.Update();
+		UpdateUniforms();
+		UpdateGameObjects();
+	}
 }
 
 void Application::ClearWindow()
 {
-    glClearColor(0.2f, 0.2f, 0.2f, 1);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.2f, 0.2f, 0.2f, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//test
 }
 
 void Application::UpdateGameObjects()
 {
-    for (int i = 0; i < GameObjectManager::GetSize(); i++)
-    {
-        GameObject* object = GameObjectManager::GetGameObject(i);
+	for (int i = 0; i < GameObjectManager::GetSize(); i++)
+	{
+		GameObject* object = GameObjectManager::GetGameObject(i);
 
-        if (object)
-        {
-            glUniformMatrix4fv(ModelLocation, 1, GL_FALSE, lm::FMat4::ToArray(object->transform.GetMatrix()));
-            object->Update();
-        }
-    }
+		if (object)
+		{
+			glUniformMatrix4fv(ModelLocation, 1, GL_FALSE, lm::FMat4::ToArray(object->transform.GetMatrix()));
+			object->Update();
+		}
+	}
 }
 
 void Application::UpdateUniforms()
 {
-    mainShader.Use();
+	mainShader.Use();
 
-    lm::FMat4 viewProj = editorCamera.GetProjectionMatrix() * editorCamera.CreateViewMatrix();
-    lm::FVec3 viewPos = editorCamera.transform.GetPosition();
+	lm::FMat4 viewProj = editorCamera.GetProjectionMatrix() * editorCamera.CreateViewMatrix();
+	lm::FVec3 viewPos = editorCamera.transform.GetPosition();
 
-    glUniformMatrix4fv(viewProjLocation, 1, GL_FALSE, lm::FMat4::ToArray(viewProj));
-    glUniform3f(viewPosLocation, viewPos.x, viewPos.y, viewPos.z);
+	glUniformMatrix4fv(viewProjLocation, 1, GL_FALSE, lm::FMat4::ToArray(viewProj));
+	glUniform3f(viewPosLocation, viewPos.x, viewPos.y, viewPos.z);
 }
