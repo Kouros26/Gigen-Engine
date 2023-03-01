2f5286d0efdb039e104c8740194715648a778be2
Angel <a.gimer@student.isartdigital.com>
2023-03-01 14:08:09 -0500

    feat: Change rotation to use Quaternions
    Change FVec3 Rotation to FQuat Rotation //Fix some weird rotation with camera

--- GigEngine/Engine/Headers/Transform.h
+++ GigEngine/Engine/Headers/Transform.h
@@ -1,40 +1,41 @@
 #pragma once
 #include "Vec3/FVec3.hpp"
 #include "Mat4/FMat4.hpp"
+#include <Quaternion/FQuat.hpp>
 
 class Transform
 {
 public:
-	Transform();
-	~Transform();
+    Transform();
+    ~Transform();
 
-	void SetPosition(const lm::FVec3& pos);
-	void SetRotation(const lm::FVec3& rot);
-	void SetScale(const lm::FVec3& scl);
+    void SetPosition(const lm::FVec3& pos);
+    void SetRotation(const lm::FQuat& rot);
+    void SetScale(const lm::FVec3& scl);
 
-	[[nodiscard]] lm::FVec3 GetPosition() const;
-	[[nodiscard]] lm::FVec3 GetScale() const;
-	[[nodiscard]] lm::FVec3 GetRotation() const;
+    [[nodiscard]] lm::FVec3 GetPosition() const;
+    [[nodiscard]] lm::FVec3 GetScale() const;
+    [[nodiscard]] lm::FQuat GetRotation() const;
 
-	void AddScale(const lm::FVec3& scl);
-	void AddRotation(const lm::FVec3& rot);
-	void AddPosition(const lm::FVec3& pos);
+    void AddScale(const lm::FVec3& scl);
+    void AddRotation(const lm::FQuat& rot);
+    void AddPosition(const lm::FVec3& pos);
 
-	lm::FVec3 GetFront();
-	lm::FVec3 GetUp();
-	lm::FVec3 GetRight();
+    lm::FVec3 GetFront();
+    lm::FVec3 GetUp();
+    lm::FVec3 GetRight();
 
-	lm::FMat4 GetMatrix();
+    lm::FMat4 GetMatrix();
 
 private:
-	void UpdateMatrix();
-	void LimitRotation();
-	//
+    void UpdateMatrix();
+    void LimitRotation();
+    //
 
-	lm::FVec3 scale = lm::FVec3(1);
-	lm::FVec3 position;
-	lm::FVec3 rotation;
-	lm::FMat4 matrix;
+    lm::FVec3 scale = lm::FVec3(1);
+    lm::FVec3 position;
+    lm::FQuat rotation = lm::FQuat(1.f);
+    lm::FMat4 matrix;
 
-	bool hasChanged = true;
+    bool hasChanged = true;
 };
--- GigEngine/Engine/Sources/Camera.cpp
+++ GigEngine/Engine/Sources/Camera.cpp
@@ -2,12 +2,12 @@
 #include "Application.h"
 
 Camera::Camera()
-	:GameObject("Camera")
+    :GameObject("Camera")
 {
-	CreateViewMatrix();
+    CreateViewMatrix();
 
-	currentRatio = Application::GetWindow().GetRatio();
-	UpdateProjectionMatrix();
+    currentRatio = Application::GetWindow().GetRatio();
+    UpdateProjectionMatrix();
 }
 
 Camera::~Camera()
@@ -16,60 +16,60 @@ Camera::~Camera()
 
 lm::FMat4 Camera::CreateViewMatrix()
 {
-	return lm::FMat4::LookAt(transform.GetPosition(), transform.GetPosition() + GetFront(), GetUp());
+    return lm::FMat4::LookAt(transform.GetPosition(), transform.GetPosition() + GetFront(), GetUp());
 }
 
 void Camera::SetFov(float fov)
 {
-	currentFov = fov;
-	UpdateProjectionMatrix();
+    currentFov = fov;
+    UpdateProjectionMatrix();
 }
 
 void Camera::SetNear(float near)
 {
-	currentNear = near;
-	UpdateProjectionMatrix();
+    currentNear = near;
+    UpdateProjectionMatrix();
 }
 
 void Camera::SetFar(float far)
 {
-	currentFar = far;
-	UpdateProjectionMatrix();
+    currentFar = far;
+    UpdateProjectionMatrix();
 }
 
 void Camera::SetRatio(float ratio)
 {
-	currentRatio = ratio;
-	UpdateProjectionMatrix();
+    currentRatio = ratio;
+    UpdateProjectionMatrix();
 }
 
 lm::FMat4 Camera::GetProjectionMatrix()
 {
-	return projectionMatrix;
+    return projectionMatrix;
 }
 
 void Camera::UpdateProjectionMatrix()
 {
-	projectionMatrix = lm::FMat4::Perspective(currentFov, currentRatio, currentNear, currentFar);
+    projectionMatrix = lm::FMat4::Perspective(currentFov, currentRatio, currentNear, currentFar);
 }
 
 lm::FVec3 Camera::GetFront()
 {
-	lm::FMat4 inverse = lm::FMat4::Inverse(transform.GetMatrix());
-	lm::FVec3 front = inverse[2];
-	return lm::FVec3::Normalize(front);
+    lm::FMat4 inverse = lm::FMat4::Inverse(transform.GetMatrix());
+    lm::FVec3 front = inverse[2];
+    return lm::FVec3::Normalize(front);
 }
 
 lm::FVec3 Camera::GetUp()
 {
-	lm::FMat4 inverse = lm::FMat4::Inverse(transform.GetMatrix());
-	lm::FVec3 up = inverse[1];
-	return lm::FVec3::Normalize(up);
+    lm::FMat4 inverse = lm::FMat4::Inverse(transform.GetMatrix());
+    lm::FVec3 up = inverse[1];
+    return lm::FVec3::Normalize(up);
 }
 
 lm::FVec3 Camera::GetRight()
 {
-	lm::FMat4 inverse = lm::FMat4::Inverse(transform.GetMatrix());
-	lm::FVec3 right = inverse[0];
-	return lm::FVec3::Normalize(right);
+    lm::FMat4 inverse = lm::FMat4::Inverse(transform.GetMatrix());
+    lm::FVec3 right = inverse[0];
+    return lm::FVec3::Normalize(right);
 }
