914c2d5612f35e9ae6ee410b3d064ba442e67c3f
Angel <a.gimer@student.isartdigital.com>
2023-03-09 10:34:21 -0500

    fix: Resolve all problem from Rotation of Camera

--- GigEngine/Editor/Sources/EditorCamera.cpp
+++ GigEngine/Editor/Sources/EditorCamera.cpp
@@ -16,112 +16,95 @@ EditorCamera::~EditorCamera()
 
 void EditorCamera::Update()
 {
-	ChangeSpeed();
-	Move();
-	Look();
+    ChangeSpeed();
+    Move();
+    Look();
 }
 
 void EditorCamera::ChangeSpeed()
 {
-	if (Inputs::GetKey(LEFT_SHIFT) && !pressLeftShift)
-	{
-		speed *= speedStep;
+    if (Inputs::GetKey(LEFT_SHIFT) && !pressLeftShift)
+    {
+        speed *= speedStep;
 
-		if (speed > speedMax)
-			speed = speedMax;
+        if (speed > speedMax)
+            speed = speedMax;
 
-		pressLeftShift = true;
-	}
+        pressLeftShift = true;
+    }
 
-	if (!Inputs::GetKey(LEFT_SHIFT))
-		pressLeftShift = false;
+    if (!Inputs::GetKey(LEFT_SHIFT))
+        pressLeftShift = false;
 
-	if (Inputs::GetKey(RIGHT_SHIFT) && !pressRightShift)
-	{
-		speed -= speedStep;
+    if (Inputs::GetKey(RIGHT_SHIFT) && !pressRightShift)
+    {
+        speed -= speedStep;
 
-		if (speed < 0)
-			speed = 0;
+        if (speed < 0)
+            speed = 0;
 
-		pressRightShift = true;
-	}
+        pressRightShift = true;
+    }
 
-	if (!Inputs::GetKey(RIGHT_SHIFT))
-		pressRightShift = false;
+    if (!Inputs::GetKey(RIGHT_SHIFT))
+        pressRightShift = false;
 }
 
 void EditorCamera::Move()
 {
-	const float scaleSpeed = speed * static_cast<float>(Time::GetDeltaTime());
+    const float scaleSpeed = speed * static_cast<float>(Time::GetDeltaTime());
 
-	if (Inputs::GetKey(UP) || Inputs::GetKey('W'))
-		transform.AddPosition(GetFront() * scaleSpeed);
+    if (Inputs::GetKey(UP) || Inputs::GetKey('W'))
+        transform.AddPosition(GetFront() * scaleSpeed);
 
-	if (Inputs::GetKey(DOWN) || Inputs::GetKey('S'))
-		transform.AddPosition(GetFront() * -scaleSpeed);
+    if (Inputs::GetKey(DOWN) || Inputs::GetKey('S'))
+        transform.AddPosition(GetFront() * -scaleSpeed);
 
-	if (Inputs::GetKey(LEFT) || Inputs::GetKey('A'))
-		transform.AddPosition(-GetRight() * -scaleSpeed);
+    if (Inputs::GetKey(LEFT) || Inputs::GetKey('A'))
+        transform.AddPosition(-GetRight() * -scaleSpeed);
 
-	if (Inputs::GetKey(RIGHT) || Inputs::GetKey('D'))
-		transform.AddPosition(-GetRight() * scaleSpeed);
+    if (Inputs::GetKey(RIGHT) || Inputs::GetKey('D'))
+        transform.AddPosition(-GetRight() * scaleSpeed);
 
-	if (Inputs::GetMouse().wheelClick)
-	{
-		transform.AddPosition(GetRight() * Inputs::GetMouse().mouseOffsetX);
-		transform.AddPosition(GetUp() * -Inputs::GetMouse().mouseOffsetY);
-	}
+    if (Inputs::GetMouse().wheelClick)
+    {
+        transform.AddPosition(GetRight() * Inputs::GetMouse().mouseOffsetX);
+        transform.AddPosition(GetUp() * -Inputs::GetMouse().mouseOffsetY);
+    }
 
-	if (Inputs::GetMouse().wheelOffsetY != 0)
-	{
-		transform.AddPosition(GetFront() * Inputs::GetMouse().wheelOffsetY);
-		Inputs::UpdateMouseWheelOffset(0);
-	}
+    if (Inputs::GetMouse().wheelOffsetY != 0)
+    {
+        transform.AddPosition(GetFront() * Inputs::GetMouse().wheelOffsetY);
+        Inputs::UpdateMouseWheelOffset(0);
+    }
 }
 
 void EditorCamera::Look()
 {
-	if (Inputs::GetMouse().rightClick == 1)
-	{
-		glfwSetInputMode(Application::GetWindow().GetGLFWWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-		const float Ry = static_cast<float>(Inputs::GetMouse().mouseOffsetX * static_cast<double>(sensitivity));
-		const float Rx = static_cast<float>(Inputs::GetMouse().mouseOffsetY * static_cast<double>(sensitivity));
+    if (Inputs::GetMouse().rightClick == 1)
+    {
+        glfwSetInputMode(Application::GetWindow().GetGLFWWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        const float Ry = static_cast<float>(Inputs::GetMouse().mouseOffsetX * static_cast<double>(sensitivity));
+        const float Rx = static_cast<float>(Inputs::GetMouse().mouseOffsetY * static_cast<double>(sensitivity));
 
-		const lm::FVec3 rot = transform.GetRotation();
+        const lm::FVec3 rot = transform.GetRotation();
 
-		lm::FQuat Rot = lm::FQuat(-Rx, Ry, 0);
+        lm::FVec3 vecRot = { -Rx, Ry, 0 };
+        transform.AddRotation(vecRot);
 
-		lm::FQuat RotX(GetRight(), -Rx);
-		lm::FQuat RotY(GetUp(), Ry);
-		lm::FQuat newRot = RotX * RotY;
-		newRot = newRot * Rot * lm::FQuat::Conjugate(newRot);
-		lm::FVec3 vecRot(newRot.x, newRot.y, newRot.z);
+        if (rot.x > maxLookAngle)
+        {
+            lm::FVec3 vecRot = { maxLookAngle, rot.y, rot.z };
+            transform.SetRotation(vecRot);
+        }
 
-		transform.AddRotation(vecRot);
+        if (rot.x < -maxLookAngle)
+        {
+            lm::FVec3 vecRot = { -maxLookAngle, rot.y, rot.z };
+            transform.SetRotation(vecRot);
+        }
+    }
 
-		if (rot.x > maxLookAngle)
-		{
-			RotX = { GetRight(), maxLookAngle };
-			RotY = { GetUp(), rot.y };
-			lm::FQuat RotZ(GetFront(), rot.z);
-			newRot = RotX * RotY * RotZ;
-			newRot = newRot * Rot * lm::FQuat::Conjugate(newRot);
-			vecRot = { newRot.x, newRot.y, newRot.z };
-			transform.SetRotation(vecRot);
-		}
-
-		if (rot.x < -maxLookAngle)
-		{
-			RotX = { GetRight(), -maxLookAngle };
-			RotY = { GetUp(), rot.y };
-			lm::FQuat RotZ(GetFront(), rot.z);
-			newRot = RotX * RotY * RotZ;
-			newRot = newRot * Rot * lm::FQuat::Conjugate(newRot);
-			vecRot = { newRot.x, newRot.y, newRot.z };
-			transform.SetRotation(vecRot);
-		}
-	}
-
-	else if (Inputs::GetMouse().rightClick == 0)
-		glfwSetInputMode(Application::GetWindow().GetGLFWWindow(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    else if (Inputs::GetMouse().rightClick == 0)
+        glfwSetInputMode(Application::GetWindow().GetGLFWWindow(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 }
--- GigEngine/Engine/Headers/Transform.h
+++ GigEngine/Engine/Headers/Transform.h
@@ -6,36 +6,37 @@
 class Transform
 {
 public:
-	Transform();
-	~Transform();
+    Transform();
+    ~Transform();
 
-	void SetPosition(const lm::FVec3& pos);
-	void SetRotation(const lm::FVec3& rot);
-	void SetScale(const lm::FVec3& scl);
+    void SetPosition(const lm::FVec3& pos);
+    void SetRotation(const lm::FVec3& rot);
+    void SetScale(const lm::FVec3& scl);
 
-	[[nodiscard]] lm::FVec3 GetPosition() const;
-	[[nodiscard]] lm::FVec3 GetScale() const;
-	[[nodiscard]] lm::FVec3 GetRotation() const;
+    [[nodiscard]] lm::FVec3 GetPosition() const;
+    [[nodiscard]] lm::FVec3 GetScale() const;
+    [[nodiscard]] lm::FVec3 GetRotation() const;
+    [[nodiscard]] lm::FQuat GetOrientation();
 
-	void AddScale(const lm::FVec3& scl);
-	void AddRotation(const lm::FVec3& rot);
-	void AddPosition(const lm::FVec3& pos);
+    void AddScale(const lm::FVec3& scl);
+    void AddRotation(const lm::FVec3& rot);
+    void AddPosition(const lm::FVec3& pos);
 
-	lm::FVec3 GetFront();
-	lm::FVec3 GetUp();
-	lm::FVec3 GetRight();
+    lm::FVec3 GetFront();
+    lm::FVec3 GetUp();
+    lm::FVec3 GetRight();
 
-	lm::FMat4 GetMatrix();
+    lm::FMat4 GetMatrix();
 
 private:
-	void UpdateMatrix();
-	void LimitRotation();
-	//
+    void UpdateMatrix();
+    void LimitRotation();
+    //
 
-	lm::FVec3 scale = lm::FVec3(1);
-	lm::FVec3 position;
-	lm::FVec3 rotation = lm::FVec3(1.f);
-	lm::FMat4 matrix;
+    lm::FVec3 scale = lm::FVec3(1);
+    lm::FVec3 position;
+    lm::FVec3 rotation = lm::FVec3(1.f);
+    lm::FMat4 matrix;
 
-	bool hasChanged = true;
+    bool hasChanged = true;
 };
