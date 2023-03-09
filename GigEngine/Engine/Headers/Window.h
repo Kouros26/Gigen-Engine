b4507c95fd226d486117a52ab23a2126b78514b4
Angel <a.gimer@student.isartdigital.com>
2023-03-09 11:31:03 -0500

    refactor: Update opengl 3.3 to 4.6

--- GigEngine/Engine/Headers/Window.h
+++ GigEngine/Engine/Headers/Window.h
@@ -6,33 +6,33 @@
 class Window
 {
 public:
-	void Init();
-	void ProcessInput() const;
+    void Init();
+    void ProcessInput() const;
 
-	static void ToggleVSync(int input);
+    static void ToggleVSync(int input);
 
-	[[nodiscard]] unsigned int GetWidth() const;
-	[[nodiscard]] unsigned int GetHeight() const;
-	[[nodiscard]] float GetRatio() const;
+    [[nodiscard]] unsigned int GetWidth() const;
+    [[nodiscard]] unsigned int GetHeight() const;
+    [[nodiscard]] float GetRatio() const;
 
-	[[nodiscard]] bool ShouldClose() const;
-	[[nodiscard]] GLFWwindow* GetGLFWWindow() const;
-	std::string& GetGLSLVersion();
+    [[nodiscard]] bool ShouldClose() const;
+    [[nodiscard]] GLFWwindow* GetGLFWWindow() const;
+    std::string& GetGLSLVersion();
 
 private:
 
-	const char* APPLICATION_NAME = "Gigen Engine";
+    const char* APPLICATION_NAME = "Gigen Engine";
 
-	GLFWwindow* window = nullptr;
-	std::string version;
-	const int versionMajor = 3;
-	const int versionMinor = 3;
+    GLFWwindow* window = nullptr;
+    std::string version;
+    const int versionMajor = 4;
+    const int versionMinor = 6;
 
-	unsigned int width = 0;
-	unsigned int height = 0;
+    unsigned int width = 0;
+    unsigned int height = 0;
 
-	static void KeyCallback(GLFWwindow*, int key, int, int action, int);
-	static void MouseButtonCallback(GLFWwindow*, int button, int action, int);
-	static void FrameBufferResizeCallback(GLFWwindow* pWindow, int width, int height);
-	static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+    static void KeyCallback(GLFWwindow*, int key, int, int action, int);
+    static void MouseButtonCallback(GLFWwindow*, int button, int action, int);
+    static void FrameBufferResizeCallback(GLFWwindow* pWindow, int width, int height);
+    static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 };
