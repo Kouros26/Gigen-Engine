35cfbaca9a6c6c4c66b030dde2d30c85d246ecfb
Ross <a.goes@student.isartdigital.com>
2023-05-16 11:58:56 -0400

    ft : audioSource file

---
+++ GigEngine/Engine/Sources/AudioSource.cpp
@@ -0,0 +1,198 @@
+#include "Light.h"
+#include "Application.h"
+#include <string>
+
+DirLight::DirLight(float ambient, float diffuse, float specular, const lm::FVec3& color)
+	:ambient(ambient), diffuse(diffuse), specular(specular)
+{
+	this->color[0] = color.x;
+	this->color[1] = color.y;
+	this->color[2] = color.z;
+}
+
+void DirLight::SendToShader(const int& pos, const std::string& shaderName)
+{
+	if (!IsActive())
+		return;
+
+	std::string str = shaderName + "[" + std::to_string(pos) + "].";
+	const std::string temp = str;
+
+	const lm::FVec3 dir = GetTransform().GetFront();
+	direction[0] = dir.x;
+	direction[1] = dir.y;
+	direction[2] = dir.z;
+
+	ShaderProgram& shader = Application::GetMainShader();
+	shader.SetVec3(direction, (str.append(g_directionShaderName)).c_str());
+	str = temp;
+	shader.SetVec3(color, (str.append(g_colorShaderName)).c_str());
+	str = temp;
+	shader.SetFloat(ambient, (str.append(g_ambientShaderName)).c_str());
+	str = temp;
+	shader.SetFloat(diffuse, (str.append(g_diffuseShaderName)).c_str());
+	str = temp;
+	shader.SetFloat(specular, (str.append(g_specularShaderName)).c_str());
+}
+
+float* DirLight::GetColor()
+{
+	return color;
+}
+
+float DirLight::GetAmbient() const
+{
+	return ambient;
+}
+
+float DirLight::GetDiffuse() const
+{
+	return diffuse;
+}
+
+float DirLight::GetSpecular() const
+{
+	return specular;
+}
+
+void DirLight::SetColor(const lm::FVec3& pColor)
+{
+	for (int i = 0; i < 3; i++)
+		color[i] = pColor[i];
+}
+
+void DirLight::SetAmbient(float pAmbient)
+{
+	ambient = pAmbient;
+}
+
+void DirLight::SetDiffuse(float pDiffuse)
+{
+	diffuse = pDiffuse;
+}
+
+void DirLight::SetSpecular(float pSpecular)
+{
+	specular = pSpecular;
+}
+
+std::string DirLight::GetType()
+{
+	const std::string type(typeid(this).name());
+	return type.substr(6, type.size() - 16);
+}
+
+PointLight::PointLight(float ambient, float diffuse, float specular, float constant, float linear, float quadratic,
+                       const lm::FVec3& color)
+	:DirLight(ambient, diffuse, specular, color), constant(constant), linear(linear), quadratic(quadratic)
+{
+}
+
+void PointLight::SendToShader(const int& pos, const std::string& shaderName)
+{
+	if (!IsActive())
+		return;
+
+	DirLight::SendToShader(pos, shaderName);
+
+	std::string str = shaderName + "[" + std::to_string(pos) + "].";
+	const std::string temp = str;
+
+	const lm::FVec3 posi = GetTransform().GetWorldPosition();
+	position[0] = posi.x;
+	position[1] = posi.y;
+	position[2] = posi.z;
+
+	ShaderProgram& shader = Application::GetMainShader();
+	shader.SetVec3(position, (str.append(g_positionShaderName)).c_str());
+	str = temp;
+	shader.SetFloat(constant, (str.append(g_constantShaderName)).c_str());
+	str = temp;
+	shader.SetFloat(linear, (str.append(g_linearShaderName)).c_str());
+	str = temp;
+	shader.SetFloat(quadratic, (str.append(g_quadraticShaderName)).c_str());
+}
+
+float PointLight::GetConstant() const
+{
+	return constant;
+}
+
+float PointLight::GetLinear() const
+{
+	return linear;
+}
+
+float PointLight::GetQuadratic() const
+{
+	return quadratic;
+}
+
+std::string PointLight::GetType()
+{
+	const std::string type(typeid(this).name());
+	return type.substr(6, type.size() - 16);
+}
+
+void PointLight::SetConstant(float pConstant)
+{
+	constant = pConstant;
+}
+
+void PointLight::SetLinear(float pLinear)
+{
+	linear = pLinear;
+}
+
+void PointLight::SetQuadratic(float pQuadratic)
+{
+	quadratic = pQuadratic;
+}
+
+SpotLight::SpotLight(float ambient, float diffuse, float specular, float constant, float linear, float quadratic, float cutOff, float outerCutOff,
+                     const lm::FVec3& color)
+	:PointLight(ambient, diffuse, specular, constant, linear, quadratic, color), cutOff(cutOff), outerCutOff(outerCutOff)
+{
+}
+
+void SpotLight::SendToShader(const int& pos, const std::string& shaderName)
+{
+	if (!IsActive())
+		return;
+
+	PointLight::SendToShader(pos, shaderName);
+
+	std::string str = shaderName + "[" + std::to_string(pos) + "].";
+	const std::string temp = str;
+
+	ShaderProgram& shader = Application::GetMainShader();
+	shader.SetFloat(cutOff, (str.append(g_cutOffShaderName)).c_str());
+	str = temp;
+	shader.SetFloat(outerCutOff, (str.append(g_outerCutOffShaderName)).c_str());
+}
+
+float SpotLight::GetCutOff() const
+{
+	return cutOff;
+}
+
+float SpotLight::GetOuterCutOff() const
+{
+	return outerCutOff;
+}
+
+std::string SpotLight::GetType()
+{
+	const std::string type(typeid(this).name());
+	return type.substr(6, type.size() - 16);
+}
+
+void SpotLight::SetCutOff(float pCutOff)
+{
+	cutOff = pCutOff;
+}
+
+void SpotLight::SetOuterCutOff(float pOuterCutOff)
+{
+	outerCutOff = pOuterCutOff;
+}
---
+++ GigEngine/Engine/include/AudioSource.h
@@ -0,0 +1,99 @@
+#pragma once
+#include "GameObject.h"
+#include "Vec3/FVec3.hpp"
+
+constexpr int g_nbMaxLight = 50;
+
+const char* const g_dirLightShaderName = "dirLights";
+const char* const g_pointLightShaderName = "pointLights";
+const char* const g_spotLightShaderName = "spotLights";
+
+const char* const g_colorShaderName = "color";
+const char* const g_directionShaderName = "direction";
+const char* const g_positionShaderName = "position";
+
+const char* const g_ambientShaderName = "ambient";
+const char* const g_diffuseShaderName = "diffuse";
+const char* const g_specularShaderName = "specular";
+
+const char* const g_constantShaderName = "constant";
+const char* const g_linearShaderName = "linear";
+const char* const g_quadraticShaderName = "quadratic";
+
+const char* const g_cutOffShaderName = "cutOff";
+const char* const g_outerCutOffShaderName = "outerCutOff";
+
+class DirLight : public GameObject
+{
+public:
+	DirLight(float ambient = 0.5f, float diffuse = 0.5f, float specular = 0.5f, const lm::FVec3& color = lm::FVec3(1));
+
+	virtual void SendToShader(const int& pos, const std::string& shaderName);
+
+	float* GetColor();
+	[[nodiscard]] float GetAmbient() const;
+	[[nodiscard]] float GetDiffuse() const;
+	[[nodiscard]] float GetSpecular() const;
+	virtual std::string GetType() override;
+
+	void SetColor(const lm::FVec3& pColor);
+	void SetAmbient(float pAmbient);
+	void SetDiffuse(float pDiffuse);
+	void SetSpecular(float pSpecular);
+
+private:
+	float direction[3]{};
+	float color[3]{};
+
+	float ambient;
+	float diffuse;
+	float specular;
+};
+
+class PointLight : public DirLight
+{
+public:
+	PointLight(float ambient = 0.5f, float diffuse = 0.5f, float specular = 0.5f,
+		float constant = 0.5f, float linear = 0.5f, float quadratic = 0.5f,
+		const lm::FVec3& color = lm::FVec3(1));
+
+	virtual void SendToShader(const int& pos, const std::string& shaderName) override;
+
+	[[nodiscard]] float GetConstant() const;
+	[[nodiscard]] float GetLinear() const;
+	[[nodiscard]] float GetQuadratic() const;
+	virtual std::string GetType() override;
+
+	void SetConstant(float pConstant);
+	void SetLinear(float pLinear);
+	void SetQuadratic(float pQuadratic);
+
+private:
+	float position[3]{};
+
+	float constant;
+	float linear;
+	float quadratic;
+};
+
+class SpotLight : public PointLight
+{
+public:
+	SpotLight(float ambient = 0.5f, float diffuse = 0.5f, float specular = 0.5f,
+		float constant = 0.5f, float linear = 0.5f, float quadratic = 0.5f,
+		float cutOff = 45, float outerCutOff = 90,
+		const lm::FVec3& color = lm::FVec3(1));
+
+	virtual void SendToShader(const int& pos, const std::string& shaderName) override;
+
+	[[nodiscard]] float GetCutOff() const;
+	[[nodiscard]] float GetOuterCutOff() const;
+	virtual std::string GetType() override;
+
+	void SetCutOff(float pCutOff);
+	void SetOuterCutOff(float pOuterCutOff);
+
+private:
+	float cutOff;
+	float outerCutOff;
+};
